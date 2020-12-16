5f593c964c985f9b105b7f7454818a4507e25d42
brigitta9 <75415492+brigitta9@users.noreply.github.com>
2020-12-16 10:55:50 +0700

    Add files via upload

---
+++ data/TITIKTUMPU_1.js
@@ -0,0 +1,8 @@
+var json_TITIKTUMPU_1 = {
+"type": "FeatureCollection",
+"name": "TITIKTUMPU_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.83153697280153, -6.176749479187054 ] } }
+]
+}
